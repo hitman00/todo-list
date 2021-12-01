bc129a7cf0827d70fd29755df2c0bcc031b19b19
hitman <mrezaee79@gmail.com>
2021-12-01 08:00:45 -0800

    remove list

--- store/todo.js
+++ store/todo.js
@@ -5,13 +5,12 @@ export const state = () => ({
 export const mutations = {
   addtodos(state, todo) {
     state.todos.push(todo)
-    // state.todos.splice(state.todos.length, 0, todo)
   },
 
   removetodos(state, id) {
     state.todos.splice(id, 1)
   },
   removeall(state) {
-    state.todos.splice(0, state.todos.length)
+    state.todos = []
   },
 }
